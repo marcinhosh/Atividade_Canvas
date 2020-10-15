8d8f7ce08f64a2edea1912a9761c07add0f43688
Marcio Vasconcelos <marcio@localhost.localdomain>
2020-10-15 16:43:10 -0300

    Second

--- code.js
+++ code.js
@@ -3,3 +3,20 @@ var canvas = document.getElementById('canvas1');
 
 // Instancia o pincel
 var pincel = canvas.getContext('2d');
+
+pincel.beginPath();
+   pincel.moveTo(50, 100);
+   pincel.lineTo(50, 300);
+   pincel.lineTo(300, 300);
+   pincel.fill();
+pincel.closePath();
+
+pincel.beginPath();
+   pincel.moveTo(300, 300);
+   pincel.lineTo(300, 300, 400, 100);
+   pincel.lineTo(300, 300, 400, 100);
+   // pincel.lineTo(300, 300);
+   // pincel.lineTo(300, 300);
+   // pincel.lineTo(400, 200);
+   pincel.stroke();
+ pincel.closePath();
